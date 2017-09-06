903afe8b7d437738f34a715b86f226fbc3d36874
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-09-06 14:15:25 -0400

    one last update before clinic

--- spec/features/dragonborn_index_page_spec.rb
+++ spec/features/dragonborn_index_page_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 feature 'user navigates to dragonborn index page' do
   let!(:dragonborn) { Dragonborn.create(name: 'Steve', gender: 'Female', race: 'Argonian')}
+  let! (:quest1) { Quest.create(quest_giver: 'The Greybeards',task: 'Find the Horn of Jurgen Windcaller', dragonborn: dragonborn) }
+  let! (:quest2) { Quest.create(quest_giver: 'Clavicus Vile', task: 'Retrieve the Rueful Axe', dragonborn: dragonborn) }
 
   scenario 'user sees all dragonborn options' do
     visit '/'
