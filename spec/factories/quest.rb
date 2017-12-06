62f88bb5b3ace6461988abe23827524d2152ddab
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-12-06 15:03:48 -0500

    strip factorybot

--- spec/factories/quest.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :quest do
-    quest_giver "John"
-    task  "Rule the world"
-    dragonborn
-  end
-end
--- spec/features/quest_new_spec.rb
+++ spec/features/quest_new_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 feature 'user adds a quest' do
-  let!(:dragonborn) { FactoryBot.create(:dragonborn) }
-  let!(:quest) { FactoryBot.create_list(:quest, 2, dragonborn: dragonborn) }
+  let!(:dragonborn) { Dragonborn.create(name: 'Jack', race: 'Orc', gender: 'Male') }
+  let!(:quest1) { Quest.create(task: "monkey around", quest_giver: 'ur mom', dragonborn: dragonborn) }
+  let!(:quest2) { Quest.create(task: "zebra around", quest_giver: 'ur dad', dragonborn: dragonborn) }
 
   scenario 'user fills out form' do
 
     visit "/dragonborns/#{dragonborn.id}"
 
     expect(page).to have_content 'Add a new Quest:'
-    # expect(page).to have_selector("input", :name =>"Task")
-    # expect(page).to have_selector("input", :name =>"Given By")
 
     fill_in 'Task', with: 'Kill the priest of Boethiah'
     fill_in 'Given By', with: 'Molag Bal'
@@ -19,7 +18,7 @@ feature 'user adds a quest' do
     expect(page.current_path).to eq("/dragonborns/#{dragonborn.id}")
     expect(dragonborn.quests.count).to eq 3
     expect(page).to have_content 'Kill the priest of Boethiah (Given by: Molag Bal)'
-    expect(page).to have_content 'Quest Added'
+    expect(page).to have_content 'New quest accepted'
   end
 
 end
